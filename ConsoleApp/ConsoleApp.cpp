072c1d30866b9a0c2f76b6aeb0d6b0c3c6b16258
Даша <@DESKTOP-QRLA3O0>
2024-10-07 19:06:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp/ConsoleApp.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <sstream>
+using namespace std;
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	float x, y, r, s;
+	cout << "Введите значение X: ";
+	cin >> x;
+	cout << "Введите значение Y: ";
+	cin >> y;
+	r = x * log10(y);
+	s = (1 / cos(x)) * (pow(y, 1 / 3));
+	cout << "Переменная R равна " << r << "\n";
+	cout << "Переменная S равна " << s << "\n";
+	float c = max(r, s);
+	cout << "Переменная C равна " << c << "\n";
+	system("pause");
+	return 0;
+}
