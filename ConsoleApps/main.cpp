381e3020db0e80b43f410f96f4dcfb4382d5d24e
Daryana <b.darya2809@gmail.com>
2024-11-12 22:37:59 +0300

    Add files via upload

--- ConsoleApps/main.cpp
+++ ConsoleApps/main.cpp
@@ -1,47 +1,46 @@
-#include<iostream>   
-#include<array>
+#include <iostream>
+#include <set>
 #include <locale>
+#include <algorithm>
 using namespace std;
 
-int main()
-{
+int main() {
 	setlocale(LC_ALL, "Russian");
-	array <int, 5> massive_A{};
-	array <int, 5> massive_B{};
-	cout << "¬ведите элементы дл€ массива A ";
-	for (int i = 0; i < massive_A.size(); i++) {
-		cin >> massive_A[i];
+	set<int> A;
+	set<int> B;
+	// ¬вод первого множества
+	cout << "¬ведите количество элементов в первом множестве ";
+	int N1;
+	cin >> N1;
+	cout << std::endl;
+	cout << "¬ведите элементы дл€ первого множества ";
+	for (int i = 0; i < N1; i++) {
+		int char1;
+		cin >> char1;
+		A.insert(char1);
 	}
-	cout << "¬ведите элементы дл€ массива B ";
-	for (int i = 0; i < massive_B.size(); i++) {
-		cin >> massive_B[i];
+	cout << std::endl;
+	cout << std::endl;
+	// ¬вод второго множества
+	cout << "¬ведите количество элементов во втором множестве ";
+	int N2;
+	cin >> N2;
+	cout << std::endl;
+	cout << "¬ведите элементы дл€ второго множества ";
+	for (int i = 0; i < N2; i++) {
+		int char2;
+		cin >> char2;
+		B.insert(char2);
 	}
-	cout << "Ёлементы массива A ";
-	for (int i : massive_A) {
-		cout << i << " ";
+	//
+	set<int> unionSet;
+	set_union(A.begin(), A.end(), B.begin(), B.end(), inserter(unionSet, unionSet.begin()));
+	cout << "ћножество, состо€щее из элементов, которые присутствуют хот€ бы в одном из множеств и €вл€ютс€ нечетными числами ";
+	cout << std::endl;
+	for (const int& element : unionSet) {
+		if (element % 2 != 0) {
+			cout << element << " ";}
 	}
-	cout << endl;
-	cout << "Ёлементы массива B ";
-	for (int i : massive_B) {
-		cout << i << " ";
-	}
-	int minimum_A;
-	minimum_A = *min_element(begin(massive_A), end(massive_A));
-	int minimum_B;
-	minimum_B = *min_element(begin(massive_B), end(massive_B));
-	for (int i = 0; i < 5; i++) {
-		massive_A[i] = massive_A[i] + minimum_A;
-		massive_B[i] = massive_B[i] + minimum_B;
-	}
-	cout << endl;
-	cout << "ѕреобразованный массив A ";
-	for (int i = 0; i < 5; i++) {
-		cout << massive_A[i] << " ";
-	}
-	cout << endl;
-	cout << "ѕреобразованный массив B ";
-	for (int i = 0; i < 5; i++) {
-		cout << massive_B[i] << " ";
-	}
+	cout << std::endl;
 	return 0;
 }
